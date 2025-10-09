684a8d3af5622aadeaf8c6b625bb39e1e9e3c8fd
Thomas Kroes <tkroes81@gmail.com>
2025-10-09 15:23:19 +0200

    Add PDBs to Conan package (#1)
    * Add debug information flag for RelWithDebInfo build
    
    * Copy PDB files to Conan package for Windows
    
    Added functionality to copy PDB files to the Conan package on Windows.

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,8 @@ import os
 import pathlib
 import subprocess
 from rules_support import PluginBranchInfo
+from conans import tools
+import shutil
 
 class MEViewConan(ConanFile):
     """Class to package using conan
@@ -155,6 +157,17 @@ class MEViewConan(ConanFile):
                 release_dir,
             ]
         )
+        
+        # Add the PDB files to the Conan package
+        if self.settings.os == "Windows":
+            print("Copying PDBs...")
+            pdb_dest = pathlib.Path(package_dir, "RelWithDebInfo", "PDBs")
+            pdb_dest.mkdir()
+            pdb_files = pdb_files = [p for p in pathlib.Path(self.build_folder).rglob('*') if p.is_file() and p.suffix.lower() == '.pdb']
+            print("PDB(s): ", pdb_files)
+            for pfile in pdb_files:
+                shutil.copy(pfile, pdb_dest)
+
         self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
