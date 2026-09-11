cc524ea8f41018668e349d9fdff47f73cb6218df
Julian Thijssen <julianthijssen@gmail.com>
2026-09-11 23:34:23 +0200

    Remove Release build

--- conanfile.py
+++ conanfile.py
@@ -128,12 +128,12 @@ class MEViewConan(ConanFile):
 
         cmake = self._configure_cmake()
         cmake.build(build_type="RelWithDebInfo")
-        cmake.build(build_type="Release")
+        #cmake.build(build_type="Release")
 
     def package(self):
         package_dir = pathlib.Path(self.build_folder, "package")
         relWithDebInfo_dir = package_dir / "RelWithDebInfo"
-        release_dir = package_dir / "Release"
+        
         print("Packaging install dir: ", package_dir)
         subprocess.run(
             [
@@ -146,23 +146,13 @@ class MEViewConan(ConanFile):
                 relWithDebInfo_dir,
             ]
         )
-        subprocess.run(
-            [
-                "cmake",
-                "--install",
-                self.build_folder,
-                "--config",
-                "Release",
-                "--prefix",
-                release_dir,
-            ]
-        )
+
         self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
         self.cpp_info.relwithdebinfo.libdirs = ["RelWithDebInfo/lib"]
         self.cpp_info.relwithdebinfo.bindirs = ["RelWithDebInfo/Plugins", "RelWithDebInfo"]
         self.cpp_info.relwithdebinfo.includedirs = ["RelWithDebInfo/include", "RelWithDebInfo"]
-        self.cpp_info.release.libdirs = ["Release/lib"]
-        self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
-        self.cpp_info.release.includedirs = ["Release/include", "Release"]
+        #self.cpp_info.release.libdirs = ["Release/lib"]
+        #self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
+        #self.cpp_info.release.includedirs = ["Release/include", "Release"]
